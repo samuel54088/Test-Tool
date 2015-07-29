8061d75023993b5db5ae596c6c1431a13682f74c
Hsien-Min Chen <samuel54088@gmail.com>
2015-07-29 18:32:35 +0800

    Initial commit

---
+++ UART_beta/UART_beta/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace UART_beta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox_Com = new System.Windows.Forms.ComboBox();
+            this.label_Com = new System.Windows.Forms.Label();
+            this.button_connect = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Send = new System.Windows.Forms.Button();
+            this.comboBox_BaudRate = new System.Windows.Forms.ComboBox();
+            this.label_BaudRate = new System.Windows.Forms.Label();
+            this.button_ComUpdate = new System.Windows.Forms.Button();
+            this.button_Disconnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox_Com
+            // 
+            this.comboBox_Com.FormattingEnabled = true;
+            this.comboBox_Com.Location = new System.Drawing.Point(32, 24);
+            this.comboBox_Com.Name = "comboBox_Com";
+            this.comboBox_Com.Size = new System.Drawing.Size(121, 20);
+            this.comboBox_Com.TabIndex = 0;
+            this.comboBox_Com.SelectedIndexChanged += new System.EventHandler(this.comboBox_Com_SelectedIndexChanged);
+            // 
+            // label_Com
+            // 
+            this.label_Com.AutoSize = true;
+            this.label_Com.Location = new System.Drawing.Point(30, 9);
+            this.label_Com.Name = "label_Com";
+            this.label_Com.Size = new System.Drawing.Size(31, 12);
+            this.label_Com.TabIndex = 1;
+            this.label_Com.Text = "COM";
+            // 
+            // button_connect
+            // 
+            this.button_connect.Location = new System.Drawing.Point(32, 216);
+            this.button_connect.Name = "button_connect";
+            this.button_connect.Size = new System.Drawing.Size(75, 23);
+            this.button_connect.TabIndex = 2;
+            this.button_connect.Text = "Connect";
+            this.button_connect.UseVisualStyleBackColor = true;
+            this.button_connect.Click += new System.EventHandler(this.button_connect_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(32, 109);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(125, 76);
+            this.listBox1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(232, 33);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(118, 99);
+            this.textBox1.TabIndex = 4;
+            // 
+            // Send
+            // 
+            this.Send.Location = new System.Drawing.Point(232, 152);
+            this.Send.Name = "Send";
+            this.Send.Size = new System.Drawing.Size(75, 23);
+            this.Send.TabIndex = 5;
+            this.Send.Text = "Send";
+            this.Send.UseVisualStyleBackColor = true;
+            this.Send.Click += new System.EventHandler(this.Send_Click);
+            // 
+            // comboBox_BaudRate
+            // 
+            this.comboBox_BaudRate.FormattingEnabled = true;
+            this.comboBox_BaudRate.Items.AddRange(new object[] {
+            "1228800"});
+            this.comboBox_BaudRate.Location = new System.Drawing.Point(32, 68);
+            this.comboBox_BaudRate.Name = "comboBox_BaudRate";
+            this.comboBox_BaudRate.Size = new System.Drawing.Size(121, 20);
+            this.comboBox_BaudRate.TabIndex = 6;
+            this.comboBox_BaudRate.SelectedIndexChanged += new System.EventHandler(this.comboBox_BaudRate_SelectedIndexChanged);
+            // 
+            // label_BaudRate
+            // 
+            this.label_BaudRate.AutoSize = true;
+            this.label_BaudRate.Location = new System.Drawing.Point(30, 53);
+            this.label_BaudRate.Name = "label_BaudRate";
+            this.label_BaudRate.Size = new System.Drawing.Size(50, 12);
+            this.label_BaudRate.TabIndex = 7;
+            this.label_BaudRate.Text = "Baud rate";
+            // 
+            // button_ComUpdate
+            // 
+            this.button_ComUpdate.Location = new System.Drawing.Point(159, 24);
+            this.button_ComUpdate.Name = "button_ComUpdate";
+            this.button_ComUpdate.Size = new System.Drawing.Size(49, 20);
+            this.button_ComUpdate.TabIndex = 8;
+            this.button_ComUpdate.Text = "Update";
+            this.button_ComUpdate.UseVisualStyleBackColor = true;
+            this.button_ComUpdate.Click += new System.EventHandler(this.button_ComUpdate_Click);
+            // 
+            // button_Disconnect
+            // 
+            this.button_Disconnect.Location = new System.Drawing.Point(123, 216);
+            this.button_Disconnect.Name = "button_Disconnect";
+            this.button_Disconnect.Size = new System.Drawing.Size(75, 23);
+            this.button_Disconnect.TabIndex = 9;
+            this.button_Disconnect.Text = "Disconnect";
+            this.button_Disconnect.UseVisualStyleBackColor = true;
+            this.button_Disconnect.Click += new System.EventHandler(this.button_Disconnect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 430);
+            this.Controls.Add(this.button_Disconnect);
+            this.Controls.Add(this.button_ComUpdate);
+            this.Controls.Add(this.label_BaudRate);
+            this.Controls.Add(this.comboBox_BaudRate);
+            this.Controls.Add(this.Send);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button_connect);
+            this.Controls.Add(this.label_Com);
+            this.Controls.Add(this.comboBox_Com);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox_Com;
+        private System.Windows.Forms.Label label_Com;
+        private System.Windows.Forms.Button button_connect;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Send;
+        private System.Windows.Forms.ComboBox comboBox_BaudRate;
+        private System.Windows.Forms.Label label_BaudRate;
+        private System.Windows.Forms.Button button_ComUpdate;
+        private System.Windows.Forms.Button button_Disconnect;
+    }
+}
+
---
+++ UART_beta/UART_beta/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace UART_beta
+{
+    public partial class Form1 : Form
+    {
+        SerialPort ComPort = new SerialPort();
+        Byte[] WriteBuffer = new Byte[11];
+
+        public Form1() //main
+        {
+            InitializeComponent();
+            button_Disconnect.Enabled = false;
+            comboBox_Com.Items.Clear();
+            String[] a = SerialPort.GetPortNames(); //回傳String Array
+            if (a.Length > 0)
+            {
+                for (int i = 0; i < a.Length; i++)
+                    comboBox_Com.Items.Add(a[i]);
+            }
+        }
+
+        private void comboBox_Com_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_connect_Click(object sender, EventArgs e)
+        {
+            ComPort.BaudRate = Convert.ToInt32(comboBox_BaudRate.Text);
+            //serialPort1.BaudRate = 9600;
+            ComPort.PortName = comboBox_Com.Text;
+            ComPort.Parity = Parity.None;   //關閉同位元檢查法
+            ComPort.DataBits = 8;   //5-8
+            ComPort.StopBits = StopBits.One;    //StopBits就是資料停止位元，代表這組資料單元到此為止(這裡使用一個停止位元)
+            ComPort.ReadTimeout = 1000;
+            ComPort.WriteTimeout = 1000;
+            if ((ComPort != null) && (!ComPort.IsOpen))
+            {
+                ComPort.Open();
+            }
+            button_connect.Enabled = false;
+            button_Disconnect.Enabled = true;
+            //serialPort1.BaudRate
+        }
+
+        private void comboBox_BaudRate_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //comboBox_BaudRate.
+        }
+
+        private void button_ComUpdate_Click(object sender, EventArgs e)
+        {
+            comboBox_Com.Items.Clear();
+            string[] a = SerialPort.GetPortNames(); //回傳String Array
+            if (a.Length > 0)
+            {
+                for (int i = 0; i < a.Length; i++)
+                    comboBox_Com.Items.Add(a[i]);
+                comboBox_Com.SelectedIndex = 0;
+            }
+            else
+            {
+
+            }
+        }
+
+        private void button_Disconnect_Click(object sender, EventArgs e)
+        {
+            ComPort.Close();
+            button_connect.Enabled = true;
+            button_Disconnect.Enabled = false;
+        }
+
+        private void Send_Click(object sender, EventArgs e)
+        {
+            WriteBuffer[0] = 0x11;
+            WriteBuffer[1] = 0x22;
+            WriteBuffer[2] = 0x01;
+            WriteBuffer[3] = 0x02;
+            WriteBuffer[4] = 0x03;
+            WriteBuffer[5] = 0x04;
+            WriteBuffer[6] = 0x05;
+            WriteBuffer[7] = ;
+
+            ComPort.Write(WriteBuffer, 0, WriteBuffer.Length);
+        }
+
+    }
+}
